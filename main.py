387f1d538f541d561a07538f527872f243025baa
Raspberry Pi <raspberrypisig@gmail.com>
2024-11-11 13:34:13 +1100

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import FreeCAD as App
 #import FreeCADGui as Gui
 import Part # type: ignore
+import os
 
 doc = App.newDocument()
 box = Part.makeBox(80, 40, 20)
 myPart = doc.addObject("Part::Feature", "myBox")
 myPart.Shape = box
+myPart.Visibility = True
 
 doc.recompute()
+filename = "box.FCStd"
+if os.path.exists(filename):
+    os.remove(filename)
 doc.saveAs("box.FCStd")
 
 
