4f9d7cab912b8e74f7bed320f6423aec55d39271
Raspberry Pi <raspberrypisig@gmail.com>
2024-11-13 09:22:57 +1100

    add functions to create doc body sketch

--- document.py
+++ document.py
@@ -7,6 +7,14 @@ def createDocument():
     sketch = doc.addObject('Sketcher::SketchObject', 'Sketch')
     return doc, body, sketch
 
+def createDocumentAndBody():
+    doc = App.newDocument()
+    body = doc.addObject('PartDesign::Body', 'Body')
+    return doc, body
+
+def createSketch(doc: App.Document, name: str):
+    sketch = doc.addObject('Sketcher::SketchObject', name)
+    return sketch
 
 def save_as(doc, fullpath):
     path = Path(fullpath)
