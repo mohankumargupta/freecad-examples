daf9e32142f0e9b7cd125a42e8b1ad1cc11e128d
Raspberry Pi <raspberrypisig@gmail.com>
2024-11-15 09:17:37 +1100

    makeRectangle first test

--- src/drawing.py
+++ src/drawing.py
@@ -1,5 +1,7 @@
+import FreeCAD as App
 from FreeCAD import Base # type: ignore
 import Part # type: ignore
+import Sketcher # type: ignore
 
 LineSegment = Part.LineSegment
 Vector2D = Base.Vector2d
@@ -69,8 +71,45 @@ down = _turtle['down']
 left = _turtle['left']
 right = _turtle['right']
 
-def makeRectangle(x, y, width, height):
-    pass
+def makeRectangle(sketch, corner, lengths):
+    hmin, hmax = corner[0], corner[0] + lengths[0]
+    vmin, vmax = corner[1], corner[1] + lengths[1]
+
+    i = int(sketch.GeometryCount)
+    sketch.addGeometry(
+        Part.LineSegment(App.Vector(hmin, vmax), App.Vector(hmax, vmax, 0))
+    )
+    sketch.addGeometry(
+        Part.LineSegment(App.Vector(hmax, vmax, 0), App.Vector(hmax, vmin, 0))
+    )
+    sketch.addGeometry(
+        Part.LineSegment(App.Vector(hmax, vmin, 0), App.Vector(hmin, vmin, 0))
+    )
+    sketch.addGeometry(
+        Part.LineSegment(App.Vector(hmin, vmin, 0), App.Vector(hmin, vmax, 0))
+    )
+
+    # add the rectangular constraints
+    sketch.addConstraint(Sketcher.Constraint("Coincident", i + 0, 2, i + 1, 1))
+    sketch.addConstraint(Sketcher.Constraint("Coincident", i + 1, 2, i + 2, 1))
+    sketch.addConstraint(Sketcher.Constraint("Coincident", i + 2, 2, i + 3, 1))
+    sketch.addConstraint(Sketcher.Constraint("Coincident", i + 3, 2, i + 0, 1))
+    sketch.addConstraint(Sketcher.Constraint("Horizontal", i + 0))
+    sketch.addConstraint(Sketcher.Constraint("Horizontal", i + 2))
+    sketch.addConstraint(Sketcher.Constraint("Vertical", i + 1))
+    sketch.addConstraint(Sketcher.Constraint("Vertical", i + 3))
+
+    # Fix the bottom left corner of the rectangle
+    sketch.addConstraint(Sketcher.Constraint("DistanceX", i + 2, 2, corner[0]))
+    sketch.addConstraint(Sketcher.Constraint("DistanceY", i + 2, 2, corner[1]))
+
+    # add dimensions
+    if lengths[0] == lengths[1]:
+        sketch.addConstraint(Sketcher.Constraint("Equal", i + 2, i + 3))
+        sketch.addConstraint(Sketcher.Constraint("Distance", i + 0, hmax - hmin))
+    else:
+        sketch.addConstraint(Sketcher.Constraint("Distance", i + 1, vmax - vmin))
+        sketch.addConstraint(Sketcher.Constraint("Distance", i + 0, hmax - hmin))
 
 
 
--- tests/drawing_rectangle_test.py
+++ tests/drawing_rectangle_test.py
@@ -1,12 +1,34 @@
 import FreeCAD as App
-import Part 
+from FreeCAD import Vector
+import Part # type: ignore
+import pytest  
 
 from drawing import makeRectangle
 
-Vector=App.Vector
+#Vector=App.Vector
 
+@pytest.fixture 
+def freecad_document(request):
+   doc_name = "boo"
+   doc = App.newDocument(doc_name)
+   yield doc
+   App.closeDocument(doc_name)
 
-def test_rectangle():
+def vector_to_tuple(vector):
+    return (vector.x, vector.y, vector.z)
+
+def line_to_tuple(line):
+  start_point = vector_to_tuple(line.StartPoint)
+  end_point = vector_to_tuple(line.EndPoint)
+  return tuple(sorted([start_point, end_point]))
+
+def two_polylines_equal(expected, actual):
+   expected_tuples = {line_to_tuple(line) for line in expected}
+   actual_tuples = {line_to_tuple(line) for line in actual}
+   assert expected_tuples == actual_tuples
+   
+
+def test_rectangle(freecad_document):
     expected_rectangle_points = [
        Vector(0,0,0),
        Vector(100,0,0),
@@ -14,12 +36,14 @@ def test_rectangle():
        Vector(0,50,0)
     ]
 
-    expected_rectangle_lines = [
+    expected_rectangle = [
       Part.LineSegment(expected_rectangle_points[i], expected_rectangle_points[(i+1)%4])
       for i in range(len(expected_rectangle_points))
     ]
 
-    rectangle = makeRectangle(0,0, 100, 50)
+    sketch = freecad_document.addObject('Sketcher::SketchObject', 'Sketch')
+    makeRectangle(sketch, (0,0), (100, 50))
+    two_polylines_equal(expected_rectangle, sketch.Geometry)
 
-    assert rectangle == expected_rectangle_lines
+    
 
